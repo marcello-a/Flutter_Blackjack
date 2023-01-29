9d3fd9308f7cfb14b73d33852db1ee035b6f4192
marcello-a <marcello.alte@gmail.com>
2023-01-29 18:53:01 +0100

    first commit

---
+++ lib/flutter_blackjack_pkg.dart
@@ -0,0 +1,7 @@
+library flutter_blackjack_pkg;
+
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/3.3.8/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/marcello/Development/03_Projects/Aktuelle-Projekte/Flutter_Blackjack/flutter_blackjack_pkg"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.0.1"
+export "FLUTTER_BUILD_NUMBER=0.0.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
